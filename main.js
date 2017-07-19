fb2c320ad67de19be6801250d73050c55bf131a5
Brandyn Sullins <woofenwauzer@gmail.com>
2017-07-19 17:44:19 -0400

    brandyn commit removing some wrong stuff

--- main.js
+++ main.js
@@ -3,28 +3,20 @@
 */
 
 // 1. First select and store the elements you'll be working with
-var url = 'http://placeholder.com';
-const musicPlayer = document.querySelector('.music-player');
-const musicPlaying = document.querySelector('#bandsong');
-const searchBox = document.querySelector('input[name=searchbox]');
-const searchButton = document.querySelector('button[name=search]');
-const resultsUl = document.querySelector('#resultsUl');
+var url = 'http://placeholder.com'
+const musicPlayer = document.querySelector('.music-player')
+const musicPlaying = document.querySelector('#bandsong')
+const searchBox = document.querySelector('input[name=searchbox]')
+const searchButton = document.querySelector('button[name=search]')
+const resultsUl = document.querySelector('#resultsUl')
 
 // 2. Create your `submit` event for getting the user's search term
 
 searchBox.addEventListener('submit', event => {
-  event.preventDefault();
-  var capture = searchBox.value;
-  url += capture;
-});
-
-// let capture = '';
-// function findSongs() {
-//   var capture = searchBox.value;
-//   console.log(capture);
-//   url += capture;
-// console.log(capture);
-// };
+  event.preventDefault()
+  var capture = searchBox.value
+  url += capture
+})
 
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
