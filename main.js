90c8b8afdaa36226d837d70c4821a869a5c50857
Jake LaFond <jakelafond@gmail.com>
2017-07-19 17:41:33 -0400

    basically done

--- main.js
+++ main.js
@@ -3,7 +3,29 @@
 */
 
 // 1. First select and store the elements you'll be working with
+var url = 'http://placeholder.com';
+const musicPlayer = document.querySelector('.music-player');
+const musicPlaying = document.querySelector('#bandsong');
+const searchBox = document.querySelector('input[name=searchbox]');
+const searchButton = document.querySelector('button[name=search]');
+const resultsUl = document.querySelector('#resultsUl');
+
 // 2. Create your `submit` event for getting the user's search term
+
+searchBox.addEventListener('submit', event => {
+  event.preventDefault();
+  var capture = searchBox.value;
+  url += capture;
+});
+
+// let capture = '';
+// function findSongs() {
+//   var capture = searchBox.value;
+//   console.log(capture);
+//   url += capture;
+// console.log(capture);
+// };
+
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
