3bee144855da71ba2a4c4fb3762f7115530c336f
Jake LaFond <jakelafond@gmail.com>
2017-07-20 13:52:22 -0400

    easy mode done

--- main.js
+++ main.js
@@ -3,20 +3,54 @@
 */
 
 // 1. First select and store the elements you'll be working with
-var url = 'http://placeholder.com'
-const musicPlayer = document.querySelector('.music-player')
-const musicPlaying = document.querySelector('#bandsong')
-const searchBox = document.querySelector('input[name=searchbox]')
-const searchButton = document.querySelector('button[name=search]')
-const resultsUl = document.querySelector('#resultsUl')
+var url = 'https://itunes.apple.com/search?term='
 
+const musicPlayer = document.querySelector('.music-player');
+const musicPlaying = document.querySelector('#bandsong');
+const searchBox = document.querySelector('input[name=searchbox]');
+const searchButton = document.querySelector('button[name=search]');
+const resultsUl = document.querySelector('#resultsUl');
 // 2. Create your `submit` event for getting the user's search term
 
 
 searchButton.addEventListener('click', event => {
   var capture = searchBox.value;
-  url += capture;
+  url += `${capture}&limit=15`;
+  while(resultsUl.firstChild){
+    resultsUl.removeChild(resultsUl.firstChild);
+    }
+
   console.log(url);
+  fetch(url).then(resp => resp.json()).then(data => {
+    console.log(data);
+    for (var i=0; i<data.results.length; i++){
+      var dataPoint = data.results[i];
+      var musicURL = dataPoint.previewUrl;
+
+      var newLi = document.createElement('li');
+
+      newLi.dataset.previewUrl = musicURL
+
+      var newImg = document.createElement('img');
+      newImg.src = dataPoint.artworkUrl100;
+      newLi.appendChild(newImg);
+
+      var newP = document.createElement('p');
+      newP.textContent = dataPoint.trackName;
+      newLi.appendChild(newP);
+
+      var newH3 = document.createElement('h3');
+      newH3.textContent = dataPoint.artistName;
+      newLi.appendChild(newH3);
+
+      newLi.addEventListener('click', event => {
+        musicPlayer.src = event.currentTarget.dataset.previewUrl
+        musicPlayer.play()
+      })
+    resultsUl.appendChild(newLi);
+    }
+  })
+  url = 'https://itunes.apple.com/search?term='
 });
 
 
